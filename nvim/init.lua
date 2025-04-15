141ee765025f6b4709965d2f594a284210cfac2a
leejunho-com <91662691+leejunho-com@users.noreply.github.com>
2025-04-15 11:34:45 +0900

    vim buffer connect system clipbaord

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- clipbaord
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight on yank and copy to system clipboard",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+    vim.fn.setreg("+", vim.fn.getreg('"'))
+  end,
+})
