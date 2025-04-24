0ea36d7be02d9e83911c6356fdd1da11fb8ba799
LEE JUN HO <mail@leejunho.com>
2025-04-24 13:47:45 +0900

    fix paste from system clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,12 +2,3 @@
 require("config.lazy")
 
 -- clipbaord
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight on yank and copy to system clipboard",
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank()
-    vim.fn.setreg("+", vim.fn.getreg('"'))
-  end,
-})
-vim.opt.clipboard = "unnamedplus"
