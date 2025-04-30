94a53676a340e39477245a38dfedd5c390f2f1ea
leejunho-com <mail@leejunho.com>
2025-04-30 17:02:35 +0900

    remove space

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,21 @@
 return {
-    {
-      "folke/tokyonight.nvim",
-  
-      opts = {
-        transparent = true,
-        styles = {
-          sidebars = "transparent",
-          floats = "transparent",
-        },
+  {
+    "folke/tokyonight.nvim",
+
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
       },
     },
-  
-    {
-      "rcarriga/nvim-notify",
-      opts = {
-        background_colour = "#000000",
-      },
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#000000",
     },
-  }
+  },
+}
+
