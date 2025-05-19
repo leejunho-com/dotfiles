9a9df56da9bbeebf62983584ee114da22bfedeb1
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2025-05-19 18:21:20 +0900

    Renewal nvim configuration from scratch

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,17 @@
-require 'core.option'
-require 'core.keymap'
+require("core.option")
+require("core.keymap")
 -- [[ install `lazy.nvim` plugin manager ]]
 --    see `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('error cloning lazy.nvim:\n' .. out)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        error("error cloning lazy.nvim:\n" .. out)
+    end
 end
 
----@type vim.option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 -- [[ configure and install plugins ]]
 --
@@ -26,10 +24,19 @@ rtp:prepend(lazypath)
 --    :lazy update
 --
 -- note: here is where you install your plugins.
-require('lazy').setup({
--- require 'plugins.test-color'
-require 'plugins.color-scheme',
-require 'plugins.neo-tree',
-require 'plugins.bufferline',
-require 'plugins.lualine',
+require("lazy").setup({
+    -- require 'plugins.test-color'
+    require("plugins.color-scheme"),
+    require("plugins.neo-tree"),
+    require("plugins.bufferline"),
+    require("plugins.lualine"),
+    require("plugins.treesitter"),
+    require("plugins.telescope"),
+    require("plugins.lsp"),
+    require("plugins.autocompletion"),
+    require("plugins.none-ls"),
+    require("plugins.gitsigns"),
+    require("plugins.snacks"),
+    require("plugins.indent-blankline"),
+    require("plugins.misc"),
 })
