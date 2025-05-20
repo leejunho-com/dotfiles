f51cf29617ce640960a8f4e5f9ee5881b7276799
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2025-05-20 21:41:02 +0900

    Fix csv highlight treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,42 +1,30 @@
 require("core.option")
 require("core.keymap")
--- [[ install `lazy.nvim` plugin manager ]]
---    see `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        error("error cloning lazy.nvim:\n" .. out)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("error cloning lazy.nvim:\n" .. out)
+	end
 end
 
 vim.opt.rtp:prepend(lazypath)
 
--- [[ configure and install plugins ]]
---
---  to check the current status of your plugins, run
---    :lazy
---
---  you can press `?` in this menu for help. use `:q` to close the window
---
---  to update plugins you can run
---    :lazy update
---
--- note: here is where you install your plugins.
 require("lazy").setup({
-    -- require 'plugins.test-color'
-    require("plugins.color-scheme"),
-    require("plugins.neo-tree"),
-    require("plugins.bufferline"),
-    require("plugins.lualine"),
-    require("plugins.treesitter"),
-    require("plugins.telescope"),
-    require("plugins.lsp"),
-    require("plugins.autocompletion"),
-    require("plugins.none-ls"),
-    require("plugins.gitsigns"),
-    require("plugins.snacks"),
-    require("plugins.indent-blankline"),
-    require("plugins.misc"),
+	-- require 'plugins.test-color'
+	require("plugins.color-scheme"),
+	require("plugins.neo-tree"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	require("plugins.none-ls"),
+	require("plugins.gitsigns"),
+	require("plugins.snacks"),
+	require("plugins.indent-blankline"),
+	require("plugins.misc"),
 })
