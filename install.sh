6408b347d5c440b204ab13dd8e010d4ff9a13aef
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2025-05-27 15:47:06 +0900

    env

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # symlink files
-ln -sf ~/.config/env-pub/zsh/zshrc ~/.zshrc
-ln -sf ~/.config/env-pub/zsh/p10k.zsh ~/.p10k.zsh
+ln -sfn ~/.config/env/public/zsh/zshrc ~/.zshrc
+ln -sfn ~/.config/env/public/zsh/p10k.zsh ~/.p10k.zsh
 
 # tmux-plugin-manager
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
