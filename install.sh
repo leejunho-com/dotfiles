5cdd12d77a97d367ac2f4d3801f0fe0ac983115d
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2025-05-27 16:19:07 +0900

    Fetch install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# env/private
+git clone https://github.com/leejunho-com/private.git ~/.config/env/private
+
 # symlink files
 ln -sfn ~/.config/env/public/zsh/zshrc ~/.zshrc
 ln -sfn ~/.config/env/public/zsh/p10k.zsh ~/.p10k.zsh
+ln -sfn ~/.config/env/public/vim/vimrc ~/.vimrc
 
 # tmux-plugin-manager
+rm -r ~/tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
