d553b5b778f722d30f57f0398f69d7d816c64f8a
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-11 23:01:49 +0900

    fix: remove backupFileExtension, clean up symlink conflicts

--- flake.nix
+++ flake.nix
@@ -39,7 +39,6 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.backupFileExtension = "bak";
               home-manager.extraSpecialArgs = { inherit user; };
               home-manager.users.${user} = {
                 imports = [
