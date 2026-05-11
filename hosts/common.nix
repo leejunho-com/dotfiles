bc7b3263d8947537b81cbe01267e9fc0ca0cd7a1
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-11 22:35:10 +0900

    init nix-darwin

---
+++ hosts/common.nix
@@ -0,0 +1,26 @@
+{ user, pkgs, ... }:
+
+{
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = [ ];
+
+  system.defaults = {
+    dock.autohide = true;
+    finder.AppleShowAllExtensions = true;
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+  };
+
+  # Used for backwards compatibility — do not change
+  system.stateVersion = 5;
+
+  users.users.${user} = {
+    name = user;
+    home = "/Users/${user}";
+  };
+}
