28ac9b70dca067107ed4661687b22657a5528644
leejunho-com <mail@leejunho.com>
2026-05-13 09:59:10 +0900

    update: flake.nix and rename modules hostname to cat

---
+++ hosts/darwin/labtop.nix
@@ -0,0 +1,18 @@
+{ user, pkgs, ... }:
+
+{
+  services.sketchybar.enable = true;
+  services.jankyborders = {
+    enable = true;
+    style = "round";
+    width = 8.0;
+    hidpi = true;
+    active_color = "0xfff37021";
+    inactive_color = "0x00000000";
+  };
+
+  services.yabai = {
+    enable = true;
+    enableScriptingAddition = true;
+  };
+}
