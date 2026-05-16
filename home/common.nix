057ff5a8e66f2c8868fdd3bb6e652937d2b8930d
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-16 19:14:37 +0900

    feat: move mpv from common to darwin/default and linux/desktop

--- home/common.nix
+++ home/common.nix
@@ -17,7 +17,7 @@ in
     iperf3 nmap wireguard-tools convmv magic-wormhole tealdeer poppler fontconfig
 
     # media
-    mpv ffmpeg ffmpegthumbnailer imagemagick mediainfo yt-dlp viu chafa resvg exiftool
+    ffmpeg ffmpegthumbnailer imagemagick mediainfo yt-dlp viu chafa resvg exiftool
 
     # ricing
     fastfetch figlet lolcat cmatrix asciinema asciinema-agg ascii-image-converter
