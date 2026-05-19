74cb19ade9f2585b3490f723875610d03c674682
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-19 23:11:00 +0900

    feat: platform-aware yazi config, nvim register keymaps, wl-clipboard

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "x", '"_x', { noremap = true, silent = true })
+
+vim.keymap.set({ "n", "x" }, "<leader>p", '"0p', { desc = "paste from yank register" })
+vim.keymap.set("x", "<leader>P", function()
+  vim.cmd('normal! "_dP')
+end, { desc = "replace without overwriting register" })
