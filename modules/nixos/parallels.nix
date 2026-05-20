6fa414210d10535858d58d04b7fa2daac91ffc42
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-20 11:26:19 +0900

    revert: remove Parallels clipboard DISPLAY workaround

--- modules/nixos/parallels.nix
+++ modules/nixos/parallels.nix
@@ -1,9 +1,4 @@
 {
   nixpkgs.config.allowUnfree = true;
   hardware.parallels.enable = true;
-
-  # Inject DISPLAY so prltoolsd can reach XWayland for clipboard sharing
-  systemd.services.prltoolsd.environment = {
-    DISPLAY = ":0";
-  };
 }
