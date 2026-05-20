bda68f80320c128727c2b49cfa53ae8ef6fc9069
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-20 16:28:47 +0900

    refactor: input_source — keylayout as en default, else ko

--- sketchybar/plugins/input_source.sh
+++ sketchybar/plugins/input_source.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 SOURCE=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources 2>/dev/null)
-if [[ "$SOURCE" == *"com.apple.inputmethod.Korean"* ]]; then
-  sketchybar --set inputsource label="ko" background.color=0xfff37021 \
-             --bar color=0x600C8FDE
-else
+
+if [[ "$SOURCE" == *"keylayout"* ]]; then
   sketchybar --set inputsource label="en" background.color=0x3f000000 \
              --bar color=0x00000000
+else
+  sketchybar --set inputsource label="ko" background.color=0xfff37021 \
+             --bar color=0x600C8FDE
 fi
