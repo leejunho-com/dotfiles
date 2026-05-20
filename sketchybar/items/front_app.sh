371251ecd85c0f370daadbd173876e41f19b3c2a
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-20 16:02:16 +0900

    revert: input_source bar tint / feat: move front_app to right

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sketchybar --add item front_app left \
+sketchybar --add item front_app right \
            --set front_app       background.color=$AERO_0 \
                                  icon.color=$WHITE \
                                  icon.font="sketchybar-app-font:Regular:16.0" \
