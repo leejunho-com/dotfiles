8d60a6949437452a971d313d357256dc51797055
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-21 14:30:14 +0900

    feat(sketchybar): show external speaker icon when no software volume control

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+source "$CONFIG_DIR/theme.sh"
+
 if [ "$SENDER" = "volume_change" ]; then
-  source "$CONFIG_DIR/theme.sh"
   VOLUME=$INFO
-
-  case $VOLUME in
-    [6-9][0-9]|100) ICON="$ICON_VOL_HIGH"  ;;
-    [3-5][0-9])     ICON="$ICON_VOL_MID"   ;;
-    [1-9]|[1-2][0-9]) ICON="$ICON_VOL_LOW" ;;
-    *)               ICON="$ICON_VOL_MUTE"  ;;
-  esac
-
-  sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
+else
+  VOLUME=$(osascript -e "output volume of (get volume settings)" 2>/dev/null)
 fi
+
+if [ -z "$VOLUME" ] || [ "$VOLUME" = "missing value" ]; then
+  sketchybar --set $NAME icon="$ICON_VOL_EXTERNAL" label=""
+  exit 0
+fi
+
+case $VOLUME in
+  [6-9][0-9]|100) ICON="$ICON_VOL_HIGH"  ;;
+  [3-5][0-9])     ICON="$ICON_VOL_MID"   ;;
+  [1-9]|[1-2][0-9]) ICON="$ICON_VOL_LOW" ;;
+  *)               ICON="$ICON_VOL_MUTE"  ;;
+esac
+
+sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
