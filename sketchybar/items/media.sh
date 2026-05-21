8c9b607cdc8327fff9681b83ff45554b552bd877
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-21 14:11:50 +0900

    refactor(sketchybar): remove redundant defaults, fix volume initial render

--- sketchybar/items/media.sh
+++ sketchybar/items/media.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 sketchybar --add item media e \
-           --set media label.color=$WHITE \
-                       label.max_chars=20 \
-                       icon.padding_left=10 \
+           --set media label.max_chars=20 \
                        scroll_texts=on \
-                       icon=􀑪             \
-                       icon.color=$WHITE   \
-                       background.drawing=on \
-                       background.color=$AERO_0 \
+                       icon=􀑪 \
                        script="$PLUGIN_DIR/media.sh" \
            --subscribe media media_change
