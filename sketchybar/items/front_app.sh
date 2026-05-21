2a2d9313313739c306b864477490a651d998d96a
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-21 10:29:35 +0900

    style(sketchybar): front_app label.padding_right=5

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -4,5 +4,6 @@ sketchybar --add item front_app q \
            --set front_app icon.background.drawing=on \
                            background.x_offset=4 \
                            background.padding_right=8 \
+                           label.padding_right=5 \
                            script="$PLUGIN_DIR/front_app.sh" \
            --subscribe front_app front_app_switched
