6146e43353adcb6ed6a55291dfab957d58ba027f
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-21 11:17:28 +0900

    feat(sketchybar): add center TrackPoint marker, fix ko label white, front_app padding

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -3,7 +3,7 @@
 sketchybar --add item front_app q \
            --set front_app icon.background.drawing=on \
                            background.x_offset=4 \
-                           background.padding_right=8 \
+                           background.padding_right=10 \
                            label.padding_right=5 \
                            script="$PLUGIN_DIR/front_app.sh" \
            --subscribe front_app front_app_switched
