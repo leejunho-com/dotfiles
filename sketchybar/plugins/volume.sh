d6bd1b1a94e5d0b5f1b8afee0d0a55d3d65f7e68
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-21 14:17:47 +0900

    revert(sketchybar): volume.sh back to event-driven only

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -2,16 +2,15 @@
 
 source "$CONFIG_DIR/theme.sh"
 
-VOLUME=$INFO
-if [ "$SENDER" != "volume_change" ]; then
-  VOLUME=$(osascript -e "output volume of (get volume settings)")
+if [ "$SENDER" = "volume_change" ]; then
+  VOLUME=$INFO
+
+  case $VOLUME in
+    [6-9][0-9]|100) ICON="$ICON_VOL_HIGH"  ;;
+    [3-5][0-9])     ICON="$ICON_VOL_MID"   ;;
+    [1-9]|[1-2][0-9]) ICON="$ICON_VOL_LOW" ;;
+    *)               ICON="$ICON_VOL_MUTE"  ;;
+  esac
+
+  sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
 fi
-
-case $VOLUME in
-  [6-9][0-9]|100) ICON="$ICON_VOL_HIGH"  ;;
-  [3-5][0-9])     ICON="$ICON_VOL_MID"   ;;
-  [1-9]|[1-2][0-9]) ICON="$ICON_VOL_LOW" ;;
-  *)               ICON="$ICON_VOL_MUTE"  ;;
-esac
-
-sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
