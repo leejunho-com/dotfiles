c826aadd95db74c34048b0efcf9bedfd4064d3ed
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-22 18:47:29 +0900

    fix(sketchybar): add update_freq=60 to volume for audio device switch

--- sketchybar/items/volume.sh
+++ sketchybar/items/volume.sh
@@ -2,5 +2,6 @@
 
 sketchybar --add item volume right \
            --set volume icon.font="$ICON_FONT" \
+                        update_freq=60 \
                         script="$PLUGIN_DIR/volume.sh" \
            --subscribe volume volume_change
