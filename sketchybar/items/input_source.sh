154278975ef804513007b58dcf456df69521ad12
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-26 20:01:29 +0900

    refactor(sketchybar): add item blur/border, remove redundant label.padding_right

--- sketchybar/items/input_source.sh
+++ sketchybar/items/input_source.sh
@@ -5,7 +5,6 @@ sketchybar --add item inputsource left \
                script="$PLUGIN_DIR/input_source.sh" \
                icon="$ICON_INPUT" \
                icon.font="$ICON_FONT" \
-               label.padding_right=10 \
            --add event input_source_changed \
                "com.apple.Carbon.TISNotifySelectedKeyboardInputSourceChanged" \
            --subscribe inputsource input_source_changed
