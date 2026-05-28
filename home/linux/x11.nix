302666d6bc198c0c7161a107d8016d662bde8f0c
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-28 15:09:29 +0900

    refactor(home): rename desktop.nix to wayland.nix, add x11.nix for i3

---
+++ home/linux/x11.nix
@@ -0,0 +1,59 @@
+{ pkgs, inputs, ... }:
+
+{
+  imports = [ inputs.xremap-nix.homeManagerModules.default ];
+
+  home.packages = with pkgs; [
+    firefox
+    ghostty
+    mpv
+    xclip
+  ];
+
+  services.xremap = {
+    enable = true;
+    withX11 = true;
+    config = {
+      keymap = [
+        {
+          name = "emacs navigation";
+          remap = {
+            "C-p" = "up";
+            "C-n" = "down";
+            "C-f" = "right";
+            "C-b" = "left";
+            "C-h" = "backspace";
+            "M-f" = "M-right";
+            "M-b" = "M-left";
+          };
+        }
+        {
+          name = "emacs editing";
+          application.not = [ "com.mitchellh.ghostty" ];
+          remap = {
+            "C-a" = "home";
+            "C-e" = "end";
+            "C-d" = "delete";
+            "C-k" = [ "S-end" "delete" ];
+            "C-u" = [ "S-home" "delete" ];
+            "C-S-f" = "S-right";
+            "C-S-b" = "S-left";
+            "C-S-n" = "S-down";
+            "C-S-p" = "S-up";
+          };
+        }
+        {
+          name = "darwin-style shortcuts";
+          application.not = [ "com.mitchellh.ghostty" ];
+          remap = {
+            "Super-c" = "C-c";
+            "Super-v" = "C-v";
+            "Super-x" = "C-x";
+            "Super-z" = "C-z";
+            "Super-a" = "C-a";
+          };
+        }
+      ];
+    };
+  };
+}
