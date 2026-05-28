872f0129f4bb2fd805e2cd3281d852d18ae6839e
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-29 00:51:19 +0900

    feat(nixos): add X200s hardware workarounds and power management

--- hosts/thinkpad-x200s/default.nix
+++ hosts/thinkpad-x200s/default.nix
@@ -2,5 +2,22 @@
     ../../modules/nixos/common.nix
     ../../modules/nixos/bios.nix
   ];
+
   boot.loader.grub.device = "/dev/sda";
+
+  # wifi firmware (intel iwlwifi)
+  hardware.enableRedistributableFirmware = true;
+
+  # acpi_osi: mute button fix
+  # nohpet: suppress NMI error on resume
+  boot.kernelParams = [ "acpi_osi=Linux" "nohpet" ];
+
+  # mei_me causes suspend errors
+  boot.blacklistedKernelModules = [ "mei" "mei_me" ];
+
+  # drm poll causes responsiveness issues on GMA 4500MHD
+  boot.extraModprobeConfig = "options drm_kms_helper poll=N";
+
+  # power management
+  services.tlp.enable = true;
 }
