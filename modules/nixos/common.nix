7c33d5ad816d32cb61154f83e6adb89e71353037
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-29 22:02:01 +0900

    feat(nixos): add fcitx5 Korean input method to common config

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -17,6 +17,15 @@
 
   programs.zsh.enable = true;
 
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-hangul
+      fcitx5-gtk
+    ];
+  };
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   system.stateVersion = "25.11";
