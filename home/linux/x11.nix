82b8e2717a9e6b90a82e30cbd7b375a92575ad6f
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-29 22:21:19 +0900

    refactor(linux): extract common.nix with xremap keymaps and fcitx5

--- home/linux/x11.nix
+++ home/linux/x11.nix
@@ -1,12 +1,9 @@
 { pkgs, inputs, ... }:
 
 {
-  imports = [ inputs.xremap-nix.homeManagerModules.default ];
+  imports = [ ./common.nix ];
 
   home.packages = with pkgs; [
-    firefox
-    ghostty
-    mpv
     xclip
     (pkgs.st.overrideAttrs (old: {
       postPatch = (old.postPatch or "") + ''
@@ -15,50 +12,5 @@
     }))
   ];
 
-  services.xremap = {
-    enable = true;
-    withX11 = true;
-    config = {
-      keymap = [
-        {
-          name = "emacs navigation";
-          remap = {
-            "C-p" = "up";
-            "C-n" = "down";
-            "C-f" = "right";
-            "C-b" = "left";
-            "C-h" = "backspace";
-            "M-f" = "M-right";
-            "M-b" = "M-left";
-          };
-        }
-        {
-          name = "emacs editing";
-          application.not = [ "com.mitchellh.ghostty" ];
-          remap = {
-            "C-a" = "home";
-            "C-e" = "end";
-            "C-d" = "delete";
-            "C-k" = [ "S-end" "delete" ];
-            "C-u" = [ "S-home" "delete" ];
-            "C-S-f" = "S-right";
-            "C-S-b" = "S-left";
-            "C-S-n" = "S-down";
-            "C-S-p" = "S-up";
-          };
-        }
-        {
-          name = "darwin-style shortcuts";
-          application.not = [ "com.mitchellh.ghostty" ];
-          remap = {
-            "Super-c" = "C-c";
-            "Super-v" = "C-v";
-            "Super-x" = "C-x";
-            "Super-z" = "C-z";
-            "Super-a" = "C-a";
-          };
-        }
-      ];
-    };
-  };
+  services.xremap.withX11 = true;
 }
