65976e7101bf63e23b03fdc205c2d07b5f8539eb
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-30 14:38:38 +0900

    refactor(linux): move xremap to gui.nix, remove blank line
    xremap requires a display backend (X11 or Wayland) so it does not
    belong in common.nix which is imported by headless Linux configs.
    Moved the xremap module import and keymap config to gui.nix.

--- home/linux/common.nix
+++ home/linux/common.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, config, ... }:
+{ pkgs, config, ... }:
 
 let
   dotfiles = "${config.home.homeDirectory}/code/dotfiles";
@@ -6,8 +6,6 @@ let
 in
 
 {
-  imports = [ inputs.xremap-nix.homeManagerModules.default ];
-
   home.file.".config/private".source = link "private";
 
   home.packages = with pkgs; [
@@ -41,60 +39,5 @@ in
     [GroupOrder]
     0=Default
   '';
-  };
-
-  services.xremap = {
-    enable = true;
-    config = {
-      keymap = [
-        {
-          name = "emacs navigation";
-          application.not = [ "com.mitchellh.ghostty" "st-256color" ];
-          remap = {
-            "C-p" = "up";
-            "C-n" = "down";
-            "C-f" = "right";
-            "C-b" = "left";
-            "C-h" = "backspace";
-            "M-f" = "M-right";
-            "M-b" = "M-left";
-          };
-        }
-        {
-          name = "emacs editing";
-          application.not = [ "com.mitchellh.ghostty" "st-256color" ];
-          remap = {
-            "C-a" = "home";
-            "C-e" = "end";
-            "C-d" = "delete";
-            "C-k" = [ "S-end" "delete" ];
-            "C-u" = [ "S-home" "delete" ];
-            "C-S-f" = "S-right";
-            "C-S-b" = "S-left";
-            "C-S-n" = "S-down";
-            "C-S-p" = "S-up";
-          };
-        }
-        {
-          name = "darwin-style shortcuts";
-          application.not = [ "com.mitchellh.ghostty" "st-256color" ];
-          remap = {
-            "Super-c" = "C-c";
-            "Super-v" = "C-v";
-            "Super-x" = "C-x";
-            "Super-z" = "C-z";
-            "Super-a" = "C-a";
-          };
-        }
-        {
-          name = "darwin-style shortcuts (terminal)";
-          application.only = [ "com.mitchellh.ghostty" "st-256color" ];
-          remap = {
-            "Super-c" = "C-S-c";
-            "Super-v" = "C-S-v";
-          };
-        }
-      ];
-    };
   };
 }
