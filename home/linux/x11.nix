b68e13ae78c610292ce762e8b023d14da2b0170d
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2026-05-30 14:30:13 +0900

    feat(st): apply ghostty color palette via postPatch awk

--- home/linux/x11.nix
+++ home/linux/x11.nix
@@ -16,6 +16,24 @@ in
     (pkgs.st.overrideAttrs (old: {
       postPatch = (old.postPatch or "") + ''
         sed -i 's/pixelsize=[0-9]*/pixelsize=25/' config.def.h
+        awk '
+        /^static const char \*colorname/ { skip=1
+          print "static const char *colorname[] = {"
+          print "\t\"#000000\", \"#c6341b\", \"#a9e15d\", \"#c9ae38\","
+          print "\t\"#749fe7\", \"#83668b\", \"#46a4a8\", \"#cccccc\","
+          print "\t\"#808080\", \"#e15140\", \"#ccfd7e\", \"#f9eb77\","
+          print "\t\"#588df6\", \"#b595b4\", \"#73e2e6\", \"#e1e1e1\","
+          print "\t[255] = 0,"
+          print "\t\"#a9e15d\","
+          print "\t\"#555555\","
+          print "\t\"#feffff\","
+          print "\t\"#000000\","
+          next
+        }
+        /^};/ && skip { skip=0; print; next }
+        skip { next }
+        { print }
+        ' config.def.h > tmp && mv tmp config.def.h
       '';
     }))
   ];
