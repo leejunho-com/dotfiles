aff1a09273a728a4e2e1f387ee14e896017fa5b5
leejunho <91662691+leejunho-com@users.noreply.github.com>
2026-06-02 20:58:17 +0900

    chore(thinkpad-x200s): remove fileSystems/swapDevices from hardware-configuration
    disko will manage filesystem declarations after reinstall with LUKS+LVM

--- hosts/thinkpad-x200s/hardware-configuration.nix
+++ hosts/thinkpad-x200s/hardware-configuration.nix
@@ -8,24 +8,6 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/8756e06d-81c5-4fa1-b25a-9ff617f7cab8";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/ba3e120a-058a-455e-83ce-b943d1f3feac";
-    fsType = "ext4";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/d009d758-f160-47c9-b0a3-3a7201f4d614";
-    fsType = "ext4";
-  };
-
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/61a96e91-500c-4ca3-baaa-a255b32211bb"; }
-  ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
