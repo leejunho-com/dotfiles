532f37ce9963744298b3ba0a1b487138ee930d46
leejunho <91662691+leejunho-com@users.noreply.github.com>
2026-06-03 17:38:56 +0900

    feat(nixos): add NFS and CIFS filesystem support

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -60,6 +60,8 @@
     "f /var/lib/systemd/linger/${user} 0644 root root -"
   ];
 
+  boot.supportedFilesystems = [ "nfs" "cifs" ];
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   system.stateVersion = "25.11";
