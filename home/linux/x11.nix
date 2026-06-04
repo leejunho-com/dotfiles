4f0d8239e8e880b079dd5b3c04ec8029fbb0381f
leejunho <91662691+leejunho-com@users.noreply.github.com>
2026-06-04 16:22:01 +0900

    feat(linux/x11): migrate from st to rxvt-unicode with OSC 52 clipboard support

--- home/linux/x11.nix
+++ home/linux/x11.nix
@@ -11,39 +11,17 @@ in
   home.file.".config/i3/config".source = link "i3/config";
   home.file.".config/i3status/config".source = link "i3/i3status.conf";
 
+  home.file.".Xresources".source = link "urxvt/Xresources";
+
   home.packages = with pkgs; [
     xclip
+    xorg.xrdb
     rofi
     pulsemixer
     alttab
     feh
     unclutter
-    (pkgs.st.overrideAttrs (old: {
-      postPatch = (old.postPatch or "") + ''
-        sed -i 's/font = "[^"]*"/font = "D2CodingLigature Nerd Font,Noto Sans CJK KR:pixelsize=25:antialias=true:autohint=true"/' config.def.h
-        sed -Ei 's/\{ TERMMOD,[[:space:]]+XK_Prior,[[:space:]]+zoom,[[:space:]]+\{\.f = \+1\} \}/{ Mod4Mask, XK_equal, zoom,      {.f = +1} }/' config.def.h
-        sed -Ei 's/\{ TERMMOD,[[:space:]]+XK_Next,[[:space:]]+zoom,[[:space:]]+\{\.f = -1\} \}/{ Mod4Mask, XK_minus, zoom,      {.f = -1} }/' config.def.h
-        sed -Ei 's/\{ TERMMOD,[[:space:]]+XK_Home,[[:space:]]+zoomreset,[[:space:]]+\{\.f =[[:space:]]+0\} \}/{ Mod4Mask, XK_0,     zoomreset, {.f =  0} }/' config.def.h
-        awk '
-        /^static const char \*colorname/ { skip=1
-          print "static const char *colorname[] = {"
-          print "\t\"#000000\", \"#c6341b\", \"#a9e15d\", \"#c9ae38\","
-          print "\t\"#749fe7\", \"#83668b\", \"#46a4a8\", \"#cccccc\","
-          print "\t\"#808080\", \"#e15140\", \"#ccfd7e\", \"#f9eb77\","
-          print "\t\"#588df6\", \"#b595b4\", \"#73e2e6\", \"#e1e1e1\","
-          print "\t[255] = 0,"
-          print "\t\"#a9e15d\","
-          print "\t\"#555555\","
-          print "\t\"#feffff\","
-          print "\t\"#000000\","
-          next
-        }
-        /^};/ && skip { skip=0; print; next }
-        skip { next }
-        { print }
-        ' config.def.h > tmp && mv tmp config.def.h
-      '';
-    }))
+    rxvt-unicode
   ];
 
   services.xremap.withX11 = true;
